30d881971655c6fe084fa40aaa3e5e2b83318706
kim thanh day ne <thanhcody9905@gmail.com>
2024-11-09 10:58:32 +0700

    Add files via upload

---
+++ KoiOrderingSystem/KoiOrderingSystem.Repositories/Interfaces/IKoiOrderDetailRepository.cs
@@ -0,0 +1,16 @@
+﻿using KoiOrderingSystem.Repositories.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystem.Repositories.Interfaces
+{
+    public interface IOrderDetailRepository
+    {
+        Task<List<OrderDetail>> GetAllOrderDetails();
+        Task<OrderDetail?> GetOrderDetailById(int orderDetailId);
+        Task<bool> AddOrderDetail(OrderDetail orderDetail);
+        Task<bool> UpdateOrderDetail(OrderDetail orderDetail);
+        Task<bool> DeleteOrderDetail(int orderDetailId);
+        Task<bool> DeleteOrderDetail(OrderDetail orderDetail);
+    }
+}
---
+++ KoiOrderingSystem/KoiOrderingSystem.Repositories/Interfaces/IKoiOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using KoiOrderingSystem.Repositories.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystem.Repositories.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<List<Order>> GetAllOrders();
+        Task<Order?> GetOrderById(int orderId);
+        Task<bool> AddOrder(Order order);
+        Task<bool> UpdateOrder(Order order);
+        Task<bool> DeleteOrder(int orderId);
+        Task<bool> DeleteOrder(Order order);
+    }
+}
