dfbfdc6c2f7808fa6026b4367ba2ab137d215273
kim thanh day ne <thanhcody9905@gmail.com>
2024-11-09 10:57:57 +0700

    Add files via upload

---
+++ KoiOrderingSystem/KoiOrderingSystem.Repositories/KoiOrderDetailRepository.cs
@@ -0,0 +1,83 @@
+﻿using KoiOrderingSystem.Repositories.Entities;
+using KoiOrderingSystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystem.Repositories
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        private readonly KoiOrderingSystemContext _context;
+
+        public OrderDetailRepository(KoiOrderingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<OrderDetail>> GetAllOrderDetails()
+        {
+            return await _context.OrderDetails.ToListAsync();
+        }
+
+        public async Task<OrderDetail?> GetOrderDetailById(int orderDetailId)
+        {
+            return await _context.OrderDetails
+                .Include(od => od.Order)
+                .Include(od => od.Product)
+                .FirstOrDefaultAsync(od => od.OrderDetailId == orderDetailId);
+        }
+
+        public async Task<bool> AddOrderDetail(OrderDetail orderDetail)
+        {
+            try
+            {
+                _context.OrderDetails.Add(orderDetail);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateOrderDetail(OrderDetail orderDetail)
+        {
+            try
+            {
+                _context.OrderDetails.Update(orderDetail);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteOrderDetail(int orderDetailId)
+        {
+            var orderDetail = await _context.OrderDetails.FindAsync(orderDetailId);
+            if (orderDetail == null) return false;
+
+            _context.OrderDetails.Remove(orderDetail);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteOrderDetail(OrderDetail orderDetail)
+        {
+            try
+            {
+                _context.OrderDetails.Remove(orderDetail);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ KoiOrderingSystem/KoiOrderingSystem.Repositories/KoiOrderRepository.cs
@@ -0,0 +1,82 @@
+﻿using KoiOrderingSystem.Repositories.Entities;
+using KoiOrderingSystem.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KoiOrderingSystem.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly KoiOrderingSystemContext _context;
+
+        public OrderRepository(KoiOrderingSystemContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetAllOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order?> GetOrderById(int orderId)
+        {
+            return await _context.Orders
+                .Include(o => o.OrderDetails)
+                .FirstOrDefaultAsync(o => o.OrderId == orderId);
+        }
+
+        public async Task<bool> AddOrder(Order order)
+        {
+            try
+            {
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateOrder(Order order)
+        {
+            try
+            {
+                _context.Orders.Update(order);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteOrder(int orderId)
+        {
+            var order = await _context.Orders.FindAsync(orderId);
+            if (order == null) return false;
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> DeleteOrder(Order order)
+        {
+            try
+            {
+                _context.Orders.Remove(order);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
