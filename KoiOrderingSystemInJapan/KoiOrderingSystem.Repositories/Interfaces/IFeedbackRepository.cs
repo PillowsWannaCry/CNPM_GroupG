937247947c2cb820c763cacd77fda4aac35a0481
JouuK5 <chautrinhbuu@gmail.com>
2024-11-20 22:49:59 +0700

    IFeedbackRepository.cs

--- KoiOrderingSystemInJapan/KoiOrderingSystem.Repositories/Interfaces/IFeedbackRepository.cs
+++ KoiOrderingSystemInJapan/KoiOrderingSystem.Repositories/Interfaces/IFeedbackRepository.cs
@@ -1,15 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using KoiOrderingSystem.Repositories.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace KoiOrderingSystem.Repositories.Interfaces
 {
     public interface IFeedbackRepository
     {
+        IQueryable<Feedback> GetAllFeedbacks();
         Task<IEnumerable<Feedback>> GetFeedbacksByCustomerIdAsync(int customerId);
         Task AddFeedbackAsync(Feedback feedback);
         Task<bool> UpdateFeedbackAsync(Feedback feedback);
         Task DeleteFeedbackAsync(int feedbackId);
+        Task<Feedback> GetFeedbackById(int id);
     }
 }
