b30fdb87816bd349231ce8133d0309e349919bfd
kim thanh day ne <thanhcody9905@gmail.com>
2024-11-20 19:13:19 +0700

    KOSinJapan

---
+++ KoiOrderingSystemInJapan/KoiOrderingSystem.Services/KoiOrderService.cs
@@ -0,0 +1,48 @@
+using KoiOrderingSystem.Repositories.Entities;
+using KoiOrderingSystem.Repositories.Interfaces;
+using KoiOrderingSystem.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiOrderingSystem.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoiOrderingSystem.Services
+{
+    public class KoiOrderService : IKoiOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public KoiOrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<List<Order>> GetAllOrdersAsync()
+        {
+            return await _orderRepository.GetAllOrders();
+        }
+
+        public async Task<Order?> GetOrderByIdAsync(int orderId)
+        {
+            return await _orderRepository.GetOrderById(orderId);
+        }
+
+        public async Task<bool> AddOrderAsync(Order order)
+        {
+            return await _orderRepository.AddOrder(order);
+        }
+
+        public async Task<bool> UpdateOrderAsync(Order order)
+        {
+            return await _orderRepository.UpdateOrder(order);
+        }
+
+        public async Task<bool> DeleteOrderAsync(int orderId)
+        {
+            return await _orderRepository.DeleteOrder(orderId);
+        }
+    }
+}
